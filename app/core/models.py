c4584551fbc8a7198658b4fff528ebe55f7b5dc3
geomario <marm1984@gmail.com>
2023-11-14 15:02:13 +0100

    refactor: minor refactor for model Participant and Wave

--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.db import models
+from typing import Dict, Any
 
 
 class UserManager(BaseUserManager):
@@ -19,18 +20,31 @@ class UserManager(BaseUserManager):
     User manager class.
     """
 
-    def create_user(self, is_active: bool = True, **extra_fields: dict) -> object:
+    def create_user(self, is_active: bool = True, **extra_fields: Any) -> "Participant":
         """
         Creates and saves a new user just with a random Token.
+
+        Args:
+            is_active (bool): User is active or not.
+            extra_fields (dict): Extra fields to be saved.
+
+        Returns:
+            object: The created user instance.
         """
         token = self.generate_token()
         user = self.model(token=token, is_active=is_active, **extra_fields)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, **extra_fields: dict) -> object:
+    def create_superuser(self, **extra_fields: Any) -> "Participant":
         """
         Creates and saves a new superuser.
+
+        Args:
+            extra_fields (dict): Extra fields to be saved.
+
+        Returns:
+            object: The created superuser instance.
         """
         user = self.create_user(**extra_fields)
         user.is_staff = True
@@ -42,6 +56,9 @@ class UserManager(BaseUserManager):
     def generate_token() -> str:
         """
         Generates a random token.
+
+        Returns:
+            str: The generated token.
         """
         alphabet = string.ascii_letters + string.digits
         return "".join(secrets.choice(alphabet) for i in range(32))
@@ -52,8 +69,10 @@ class Participant(AbstractBaseUser, PermissionsMixin):
     Custom participant model.
     """
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    token = models.CharField(max_length=32, unique=True)
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, blank=False
+    )
+    token = models.CharField(max_length=32, unique=True, blank=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -65,29 +84,12 @@ class Participant(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self) -> str:
         """
-        Returns the string representation of the user.
+        Returns the string representation of the participant.
+
+        Returns:
+            str: The token of the participant.
         """
-        return self.token
-
-
-class Wave(models.Model):
-    """
-    Custom wave model.
-    """
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    client_id = models.CharField(max_length=32)
-    wave_number = models.IntegerField()
-    wave_name = models.CharField(max_length=32)
-    wave_type = models.CharField(max_length=32)
-    wave_status = models.CharField(max_length=32)
-    participants = models.ManyToManyField(Participant, through="WaveParticipant")
-
-    def __str__(self) -> str:
-        return f"{self.client_id} - {self.wave_number} - {self.start_date} to {self.end_date}"
+        return str(self.token)
 
 
 class WaveParticipant(models.Model):
@@ -96,5 +98,34 @@ class WaveParticipant(models.Model):
     """
 
     participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
-    wave = models.ForeignKey(Wave, on_delete=models.CASCADE)
-    addtional_field = models.CharField(max_length=32)
+    wave = models.ForeignKey("Wave", on_delete=models.CASCADE)
+    additional_field = models.CharField(max_length=32)
+
+
+class Wave(models.Model):
+    """
+    Custom wave model.
+    """
+
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, blank=False
+    )
+    start_date = models.DateTimeField(blank=False)
+    end_date = models.DateTimeField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, blank=False)
+    client_id = models.CharField(max_length=32, blank=False)
+    wave_number = models.IntegerField(blank=False)
+    wave_name = models.CharField(max_length=32, blank=False)
+    wave_type = models.CharField(max_length=32, blank=False)
+    wave_status = models.CharField(max_length=32, blank=False)
+    participants = models.ManyToManyField(Participant, through="WaveParticipant")
+
+    def __str__(self) -> str:
+        """
+        Returns the string representation of the wave.
+
+        Returns:
+            str: A formatted string with wave information.
+        """
+
+        return f"{self.client_id} - {self.wave_number} - {self.start_date} to {self.end_date}"
