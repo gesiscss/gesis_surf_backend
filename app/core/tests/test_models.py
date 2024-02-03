ed9223ef637d4b7b0afa1a49a21403801aaa1e7b
geomario <marm1984@gmail.com>
2024-02-03 22:39:24 +0100

    refactor: modifying all test for functional API with tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,26 +25,19 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
         self.assertFalse(user.is_staff)
 
-    # def test_new_user_invalid_user_id(self) -> None:
-    #     """
-    #     Tests creating a new user with no user_id raises an error.
-    #     """
-    #     with self.assertRaises(ValueError):
-    #         get_user_model().objects.create_user(user_id="", token_gesis="test123")
+    def test_new_user_invalid_user_id(self) -> None:
+        """
+        Tests creating a new user with no user_id raises an error.
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(user_id="", password="test123")
 
-    # def test_create_superuser(self) -> None:
-    #     """
-    #     Tests creating a new superuser.
-    #     """
-    #     user = get_user_model().objects.create_superuser(
-    #         user_id="test", token_gesis="test123"
-    #     )
-    #     self.assertTrue(user.is_superuser)
-    #     self.assertTrue(user.is_staff)
-
-    # def test_str_method(self) -> None:
-    #     """
-    #     Tests the __str__ method.
-    #     """
-    #     user = get_user_model().objects.create_user(token="test")
-    #     self.assertEqual(str(user), user.token)
+    def test_create_superuser(self) -> None:
+        """
+        Tests creating a new superuser.
+        """
+        user = get_user_model().objects.create_superuser(
+            user_id="test", password="test123"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
