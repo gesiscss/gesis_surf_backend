f418ad670b97143ff183c0cd0353d9ccadc7fde7
geomario <marm1984@gmail.com>
2024-02-11 23:33:25 +0100

    refactor: small modifications to pre-commit

--- app/user/views.py
+++ app/user/views.py
@@ -3,23 +3,20 @@ Views for the user app.
 """
 
 from rest_framework import authentication
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.response import Response
 
 from user.serializers import AuthTokenSerializer
-from user.serializers import UserSerializer, WaveSerializer
-
-from core.models import User
+from user.serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """
     Create a new user in the system.
     """
-    queryset = User.objects.all()
+
     serializer_class = UserSerializer
 
 
