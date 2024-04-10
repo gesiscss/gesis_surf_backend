1f2799c4fc7977f25ca66141e58fe725df1c43da
geomario <marm1984@gmail.com>
2024-04-11 00:42:12 +0200

    build: celery worker with creation endpoint and fetching endpoint

--- app/host/urls.py
+++ app/host/urls.py
@@ -13,4 +13,8 @@ app_name = "host"
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "task-result/<str:task_id>/",
+        views.HostViewSet.as_view({"get": "get_async_task_result"}),
+    ),
 ]
--- app/host/views.py
+++ app/host/views.py
@@ -2,11 +2,16 @@
 Views for the host app.
 """
 
+from celery.result import AsyncResult
 from core.models import Host
+from django.http import JsonResponse
 from host import serializers
+from host.tasks import get_hosts_async
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class HostViewSet(viewsets.ModelViewSet):
@@ -28,4 +33,23 @@ class HostViewSet(viewsets.ModelViewSet):
         """
         Return objects for the current authenticated user only.
         """
-        return self.queryset.filter().order_by("-id")
+        return self.queryset.filter()
+
+    @action(detail=False, methods=["get"])
+    def async_hosts(self, request):
+        """
+        Trigger an asynchronous task to fetch hosts and return the task ID.
+        """
+        task = get_hosts_async.delay()
+        return Response({"task_id": task.id})
+
+    def get_async_task_result(self, request, task_id):
+        """
+        Endpoint to get the result of an asynchronous task by its task ID.
+        """
+        task_result = AsyncResult(task_id)
+        if task_result.ready():
+            # Task has finished, parse the JSON result and return it
+            return JsonResponse(task_result.result, safe=False, status=200)
+        # Task is still processing
+        return JsonResponse({"status": "Processing"}, status=202)
