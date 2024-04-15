4efea82d18aaa9db92596e199e03cd08ca1833e9
geomario <marm1984@gmail.com>
2024-04-15 12:20:11 +0200

    build: production ready async task

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,5 +5,8 @@ set -e
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
+celery -A app worker -l info
+celery -A app flower --port=5555
+
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
