d3871505eb7ca936eef9dcf64fcc55162bc99b81
geomario <marm1984@gmail.com>
2024-09-03 20:50:45 +0200

    build: eltk for consuming logs, missing beats

--- app/app/settings.py
+++ app/app/settings.py
@@ -152,3 +152,29 @@ REST_FRAMEWORK = {
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
+
+# Logstash configuration
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "logstash": {
+            "level": "DEBUG",
+            "class": "logstash.TCPLogstashHandler",
+            "host": "logstash",
+            "port": 5044,
+            "version": 1,
+            "message_type": "logstash",
+            "fqdn": False,
+            "tags": ["app"],
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["logstash"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
