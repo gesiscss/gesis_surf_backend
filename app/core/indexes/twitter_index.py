2853101eb6d9ac345bdc7671746152a077140ced
Geomario <marm1984@gmail.com>
2024-12-31 18:53:05 -0600

    buidl: twitter document and index

---
+++ app/core/indexes/twitter_index.py
@@ -0,0 +1,26 @@
+"""
+Document for twitter index
+"""
+
+from elasticsearch_dsl import Text, Keyword, Date
+from .base_index import BaseIndex
+
+
+class TwitterIndex(BaseIndex):
+    """Document for twitter index
+
+    Args:
+        BaseIndex (_type_): _description_
+    """
+
+    tweet_id = Keyword()
+    username = Keyword()
+    content = Text()
+    timestamp = Date()
+
+    class Index:
+        """
+        Default settings for all indexes
+        """
+
+        name = "twitter_index"
