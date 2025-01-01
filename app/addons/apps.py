c77853c6b024754399899e58f65cc40a553693e1
Geomario <marm1984@gmail.com>
2025-01-01 13:06:19 -0600

    build: addons app copnfiguration to initilize addons documents

--- app/addons/apps.py
+++ app/addons/apps.py
@@ -1,6 +1,39 @@
+"""
+App configuration for the addons app.
+"""
+
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 
 class AddonsConfig(AppConfig):
+    """_description_
+
+    Args:
+        AppConfig (_type_): _description_
+    """
+
     default_auto_field = "django.db.models.BigAutoField"
     name = "addons"
+
+    def ready(self) -> None:
+        """_summary_"""
+        from core.indexes.chatgpt_index import (  # pylint: disable=import-outside-toplevel
+            ChatGPTIndex,
+        )
+        from core.indexes.twitter_index import (  # pylint: disable=import-outside-toplevel
+            TwitterIndex,
+        )
+
+        @receiver(post_migrate)
+        def create_elasticsearch_indices(
+            sender: object, **kwargs: object  # pylint: disable=unused-argument
+        ) -> None:
+            """Starts the Elasticsearch indices as needed
+
+            Args:
+                sender (_type_): _description_
+            """
+            ChatGPTIndex.init()
+            TwitterIndex.init()
