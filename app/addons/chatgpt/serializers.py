329e2f32c6dab7bca03cca255e239302c679ff3b
Geomario <marm1984@gmail.com>
2025-01-05 21:00:16 -0600

    refactor: add post and update to serializer in chatgpt serializer

--- app/addons/chatgpt/serializers.py
+++ app/addons/chatgpt/serializers.py
@@ -14,3 +14,15 @@ class ChatGPTDataSerializer(serializers.Serializer):
     conversation_id: str = serializers.CharField()
     conversation: str = serializers.CharField()
     timestamp: str = serializers.DateTimeField()
+
+    def create(self, validated_data: dict) -> object:
+        """
+        Create a new ChatGPTData instance.
+        """
+        raise NotImplementedError("We handle creation in the View.")
+
+    def update(self, instance: object, validated_data: dict) -> object:
+        """
+        Update an existing ChatGPTData instance.
+        """
+        raise NotImplementedError("We handle updating in the View.")
