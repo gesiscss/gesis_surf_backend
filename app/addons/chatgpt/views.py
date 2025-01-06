d1d1167ad49621058a830c161ff7ec04905a765c
Geomario <marm1984@gmail.com>
2025-01-05 21:44:30 -0600

    build: api view for chatgpt

--- app/addons/chatgpt/views.py
+++ app/addons/chatgpt/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+"""
+Views for the ChatGPT addon.
+"""
 
-# Create your views here.
+from chatgpt import serializers
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class ChatGPTDataView(APIView):
+    """
+    API view for ChatGPT data.
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = serializers.ChatGPTDataSerializer
+
+    def post(self, request) -> Response:
+        """
+        Create a new ChatGPT data object.
+        """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
