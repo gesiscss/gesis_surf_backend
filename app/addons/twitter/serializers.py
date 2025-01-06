ecbc41cf7cc08120dda68646c190a98f903e0464
Geomario <marm1984@gmail.com>
2025-01-05 20:59:14 -0600

    build: serializer for twitter addon

--- app/addons/twitter/serializers.py
+++ app/addons/twitter/serializers.py
@@ -1,16 +1,28 @@
 """
-Serializers for the Addons APIs.
+Serializers for Twitter data.
 """
 
 from rest_framework import serializers
 
 
-class ChatGPTDataSerializer(serializers.Serializer):
+class TwitterDataSerializer(serializers.Serializer):
     """
-    Serializer for the ChatGPT data object.
+    Serializer for Twitter data.
     """
 
-    user_id: str = serializers.CharField()
-    conversation_id: str = serializers.CharField()
-    conversation: str = serializers.CharField()
-    timestamp: str = serializers.DateTimeField()
+    tweet_id = serializers.CharField(max_length=100)
+    username = serializers.CharField(max_length=100)
+    content = serializers.CharField()
+    timestamp = serializers.DateTimeField()
+
+    def create(self, validated_data: dict) -> object:
+        """
+        Create a new TwitterData instance.
+        """
+        raise NotImplementedError("We handle creation in the View.")
+
+    def update(self, instance: object, validated_data: dict) -> object:
+        """
+        Update an existing TwitterData instance.
+        """
+        raise NotImplementedError("We handle updating in the View.")
