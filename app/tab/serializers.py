5b74be0130244a9bf1af4be56cd00d1ce8bfa326
Geomario <marm1984@gmail.com>
2025-09-07 19:52:47 +0200

    build: creating helper functions and adapting serializers to handle creation, patch and attachment of id in API Tab

--- app/tab/serializers.py
+++ app/tab/serializers.py
@@ -11,7 +11,7 @@ class DomainSerializer(serializers.ModelSerializer):
     Serializer for the domain object.
     """
 
-    class Meta:
+    class Meta:  # type: ignore
         """
         Meta class for the domain
         """
@@ -43,7 +43,15 @@ class TabSerializer(serializers.ModelSerializer):
 
     domains = DomainSerializer(many=True, required=False)
 
-    class Meta:
+    domain_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Domain.objects.all(),
+        write_only=True,
+        required=False,
+        source="domains",
+    )
+
+    class Meta:  # type: ignore
         """
         Meta class for the tab
         """
@@ -59,6 +67,7 @@ class TabSerializer(serializers.ModelSerializer):
             "tab_num",
             "tab_session_id",
             "domains",
+            "domain_ids",
         ]
 
         # Able to change the fields, but not the id.
@@ -75,13 +84,50 @@ class TabSerializer(serializers.ModelSerializer):
             domain_obj = Domain.objects.create(user=auth_user, **domain)
             tab.domains.add(domain_obj)
 
+    def _handle_domains(self, domains: list, tab: Tab) -> None:
+        """
+        Handle the domains for the tab.
+        """
+        if domains and hasattr(domains[0], "id"):
+            for domain in domains:
+                tab.domains.add(domain)
+        else:
+            self._create_domains(domains, tab)
+
+    def _update_domains(self, domain_updates: list, tab: Tab) -> None:
+        """
+        Update the domains for the tab.
+        """
+        auth_user = self.context["request"].user
+
+        for domain_data in domain_updates:
+
+            domain_id = domain_data.get("id")
+
+            if domain_id:
+                try:
+                    domain = Domain.objects.get(id=domain_id, user=auth_user)
+                    for attr, value in domain_data.items():
+                        setattr(domain, attr, value)
+                    domain.save()
+
+                    if domain not in tab.domains.all():
+                        tab.domains.add(domain)
+
+                except Domain.DoesNotExist:
+                    continue
+
     def create(self, validated_data):
         """
         Create a new tab even with duplicated domains.
         """
         domains = validated_data.pop("domains", [])
         tab = Tab.objects.create(**validated_data)
-        self._create_domains(domains, tab)
+
+        # Hanlde domains if any
+        if domains:
+            self._handle_domains(domains, tab)
+
         return tab
 
     def update(self, instance: Tab, validated_data: dict) -> Tab:
@@ -89,12 +135,17 @@ class TabSerializer(serializers.ModelSerializer):
         Update the tab object.
         """
         domains: list = validated_data.pop("domains", None)
-
-        if domains is not None:
-            self._create_domains(domains, instance)
+        domain_updates: list = validated_data.pop("domain_updates", None)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
+
+        if domains is not None:
+            instance.domains.clear()
+            self._handle_domains(domains, instance)
+
+        if domain_updates is not None:
+            self._update_domains(domain_updates, instance)
 
         instance.save()
         return instance
